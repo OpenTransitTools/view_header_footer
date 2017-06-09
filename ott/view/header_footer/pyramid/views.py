254d722f1655a94a049acec211ec88923e4777b8
Frank Purcell <fxpurcell@gmail.com>
2017-06-08 21:15:06 -0700

    mini refactor of the mobile stuff .. fix header plus other

--- ott/view/header_footer/pyramid/views.py
+++ ott/view/header_footer/pyramid/views.py
@@ -56,7 +56,7 @@ def example(request):
     if do_fieldtrip:
         import requests
         c = requests.get("http://fieldtrip.trimet.org/fieldtrip/newRequestForm")
-        c = c.text[880:-14]
+        c = c.text[880:]
         ht = html_utils.html_escape("Field Trip Request Form")
         st = html_utils.html_escape("Complete and return this form to request a field trip if you wish to travel on TriMet with a group of 15 or more. __Trips **must*** be scheduled and paid for (if applicable) at least two weeks in advance___. If you have any questions, please email us at fieldtrips@trimet.org or call 503-962-2424, option 4.")
     else:
